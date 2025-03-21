af98fe23d075ea20e3478e8d1e15eebd63018558
tonyatpeking <tonyatpeking@gmail.com>
2025-03-20 19:44:03 -0500

    ⚗️ experimenting with the windows clipboard

--- README.md
+++ README.md
@@ -11,3 +11,8 @@
 - Prompt: A 2D cartoonish pirate with an eye patch, wearing a striped shirt and a bandana, he is an amputee missing a hand, that hand is replaced by a pirate hook. He has a life-saver tucked under his arm. He is tiling a bathroom wall. The bathroom wall is partially tiled with blue tone tiles that make a beautiful pattern, and the pirate is happily working with a big smile. There is a stack of tiles on the floor yet to be tiled. The style is flat 2D, colorful, and playful, with clean lines and vibrant colors, creating a cheerful atmosphere. The pirate looks fun and friendly, enjoying his tiling task.
 - Select hook for edit.
 - Prompt: instead of a hook attachment, it should be a trowel attachment to fit the theme of a pirate tiling a bathroom wall. Do not try to generate hands, the pirate is an amputee and the trowel is a tool attachment for his stump.
+
+## Packages to look at
+
+- pathspec
+- pyperclip
--- src/pywet/__init__.py
+++ src/pywet/__init__.py
@@ -11,19 +11,40 @@ import win32gui
 import win32process
 
 
+def get_clipboard_formats():
+    l = [win32clipboard.EnumClipboardFormats(0)]
+    while l[-1]:
+        l.append(win32clipboard.EnumClipboardFormats(l[-1]))
+    l.pop()
+    ret = {}
+    for e in l:
+        try:
+            name = win32clipboard.GetClipboardFormatName(e)
+        except:
+            name = ""
+        ret[e] = name
+    return ret
+
+
+def get_clipboard_drop_files():
+    handle = win32clipboard.GetClipboardData(win32clipboard.CF_HDROP)
+    # filename = win32api.DragQueryFile(handle, 0)
+    return handle
+
+
 def main() -> None:
     print("Hello from pywet!")
     # win32api.ClipCursor((100, 100, 200, 200))
     # win32api.Beep(1000, 1000)  #
     # win32api.ClipCursor((0, 0, 0, 0))
     # for G9 ultrawide top left is (0,0), bottom right is (5119, 1439)
-    print(win32api.GetCursorPos())
-    print(win32api.GetDiskFreeSpaceEx("C:\\"))
+    # print(win32api.GetCursorPos())
+    # print(win32api.GetDiskFreeSpaceEx("C:\\"))
     # device name
-    print(win32api.GetDomainName())
-    print(win32api.GetKeyboardState())
+    # print(win32api.GetDomainName())
+    # print(win32api.GetKeyboardState())
     # TIL that windows 11 is still windows 10
-    print(win32api.GetVersionEx())
+    # print(win32api.GetVersionEx())
 
     # Play the system exclamation sound
     # win32api.MessageBeep(win32con.MB_ICONEXCLAMATION)
@@ -34,9 +55,12 @@ def main() -> None:
     # move cursor
     # win32api.SetCursorPos((100, 100))
     win32clipboard.OpenClipboard()
-    print(win32clipboard.GetClipboardData())
-    win32clipboard.SetClipboardText(
-        "Helloooooooooooooo", win32clipboard.CF_UNICODETEXT)
+    for k, v in get_clipboard_formats().items():
+        print(k, v)
+    print(get_clipboard_drop_files())
+    # print(win32clipboard.GetClipboardData())
+    # win32clipboard.SetClipboardText(
+    #    "Helloooooooooooooo", win32clipboard.CF_UNICODETEXT)
     win32clipboard.CloseClipboard()
 
     # get foreground window
@@ -51,4 +75,6 @@ def main() -> None:
         thread_id, process_id = win32process.GetWindowThreadProcessId(hwnd)
         print(hex(hwnd), f'Thread ID: {thread_id}, Process ID: {process_id}')
 
-    win32gui.EnumWindows(win_enum_callback, None)
+    # win32gui.EnumWindows(win_enum_callback, None)
+    # import pyperclip
+    # print(pyperclip.paste())
